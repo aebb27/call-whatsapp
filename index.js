c59258c8b24e33a3fe955ccb3e06f1bc5e9811a3
aebb27 <arielbatista37@gmail.com>
2021-12-29 22:01:00 -0500

    Functionality ready

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const args = process.argv;
-let phone = args[2];
-const api = `https://api.whatsapp.com/send?phone=507${phone}`;
-if (phone === undefined) {
-	console.log('No ha ingresado un número');
-} else if (phone.length < 8 || phone.length > 8) {
-	console.error('Número de teléfono incompleto');
-} else {
-	console.log(api);
-}
+const input = document.querySelector('#phone');
+const link = document.querySelector('a');
+const helperNode = document.querySelector('.helper');
+const signal = document.querySelector('.signal');
+input.addEventListener('change', () => {
+	let phone = input.value;
+	console.log(phone.length);
+	if (phone === undefined) {
+		helperNode.innerText = 'Número no introducido.';
+		helperNode.classList.remove('hide');
+		signal.classList.remove('ready');
+	} else if (phone.length < 8 || phone.length > 8) {
+		helperNode.innerText = 'Formato Inválido.';
+		helperNode.classList.remove('hide');
+		signal.classList.remove('ready');
+	} else {
+		helperNode.classList.add('hide');
+		signal.classList.add('ready');
+		const api = `https://api.whatsapp.com/send?phone=507${phone}`;
+		link.href = api;
+	}
+});
